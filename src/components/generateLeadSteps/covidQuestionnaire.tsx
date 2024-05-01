d5386c349b4c1d4f987a449729be9b92d756d42c
jatin-dev-meesra <jatin.meesra.com>
2024-05-01 16:19:34 +0530

    declaration page

--- src/components/generateLeadSteps/covidQuestionnaire.tsx
+++ src/components/generateLeadSteps/covidQuestionnaire.tsx
@@ -4,6 +4,64 @@ const CovidQuestionnaire = () => {
   return (
     <div className="bg-white border-2 px-8 py-6 border-solid rounded-lg">
       <h1 className=" text-xl uppercase font-medium">Covid-19</h1>
+      <div className="py-5">
+        <ul className="list-disc list-inside">
+          <li className="flex items-start gap-2 flex-col lg:flex-row">
+            <p className="text-sm">
+              Currently or in the last 3 months have you or your family members
+              been tested positive for Covid 19/Have been self isolated with
+              symptoms on medical advice/ Yes No advised to undergo, repeat or
+              awaiting Covid 19 test/ Do you currently or in the past 1 month
+              have symptoms like persistent cough, breathlessness, fever, raised
+              temperature or flu like symptoms / Been in contact with an
+              individual suspected or quarantined or confirmed to have COVID-19
+              or Sars cov-2/ or does your or immediate family members occupation
+              require you/them to come in close contact with COVID-19 patients
+              or with coronavirus contaminated material?
+            </p>
+            <div className="py-1 flex gap-3">
+              <button
+                className="bg-m-orange text-center rounded-md text-white px-2 py-1 text-sm"
+                onClick={() => {}}
+              >
+                Yes
+              </button>
+              <button
+                className={`border border-m-orange text-center rounded-md text-m-orange px-2 py-1 text-sm`}
+                onClick={() => {
+                  alert("under Development :)");
+                }}
+              >
+                No
+              </button>
+            </div>
+          </li>
+          <li className="flex items-start gap-2 flex-col lg:flex-row">
+            <p className="text-sm">
+              Have you or your immediate family members travelled overseas in
+              the last 45 days OR plan to travel outside India during the next 6
+              months? Please provide complete details in the declaration given
+              below, if above question is answered Yes:
+            </p>
+            <div className="py-1 flex gap-3">
+              <button
+                className="bg-m-orange text-center rounded-md text-white px-2 py-1 text-sm"
+                onClick={() => {}}
+              >
+                Yes
+              </button>
+              <button
+                className={`border border-m-orange text-center rounded-md text-m-orange px-2 py-1 text-sm`}
+                onClick={() => {
+                  alert("under Development :)");
+                }}
+              >
+                No
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/generateLeadSteps/declaration.tsx
+++ src/components/generateLeadSteps/declaration.tsx
@@ -4,6 +4,67 @@ const Declaration = () => {
   return (
     <div className="bg-white border-2 px-8 py-6 border-solid rounded-lg">
       <h1 className=" text-xl font-medium">Declaration</h1>
+      <div className="text-sm pt-5">
+        <p>
+          I here by declare that the information provided in the above
+          questionnaire is true to the best of my knowledge. I confirm that the
+          answers I have given are, to the best of my knowledge, true, and that
+          have not withheld any material information that may influence the
+          assessment or acceptance of this application. I agree that this form
+          will constitute part of my application for insurance (s) and that
+          failure to disclose any material fact know to me may invalidate my
+          insurance(s).
+        </p>
+        <br />
+        <p>
+          I/We fully understand that any personal information collected or held
+          by the Bajaj Allianz Life Insurance Company Limited {`("Company")`}
+          (whether contained in this application or otherwise obtained) may be
+          held, used and disclosed by the Company to reinsurance companies,
+          claims investigation agencies, credit/claim/fraud bureaus or service
+          providers or repositories and relevant industry
+          associations/federations for the purpose of underwriting or claims
+          processing or for any analysis.
+        </p>
+        <br />
+        <p>
+          I/We am/are aware that the policy shall be governed by the Terms &
+          Conditions of the policy issued by BAJAJ ALLIANZ LIFE INSURANCE CO.
+          LTD. Pursuant to the proposal for insurance made by us. I/We have
+          independently verified the information before making my/our decision.
+        </p>
+        <br />
+        <p>
+          I/We am/are aware that the GCPP policy taken by me/us, is issued and
+          underwritten by BAJAJ ALLIANZ LIFE INSURANCE CO. LTD., and that all
+          claims will be settled BAJAJ ALLIANZ LIFE INSURANCE CO. LTD., as per
+          the terms and conditions of the policy.
+        </p>
+        <br />
+        <p>
+          I/We hereby confirm that I/We have agreed to subscribe to the policy
+          purely on a voluntary basis after taking my/our independent
+          professional advise and liable for any liability for loss or damage of
+          whatsoever nature, which may be attributable to payment of claims
+          under the policy of Insurance.
+        </p>
+        <br />
+        <p>
+          The above declaration and other details are true to best of my
+          knowledge. I have been explained the rules of the scheme and have
+          understood them.
+        </p>
+      </div>
+      <div className="py-5">
+        <button
+          className="bg-m-orange text-center rounded-md w-full text-white px-2 py-1 text-sm"
+          onClick={() => {
+            // handleClaculate();
+          }}
+        >
+          I Agree
+        </button>
+      </div>
     </div>
   );
 };
