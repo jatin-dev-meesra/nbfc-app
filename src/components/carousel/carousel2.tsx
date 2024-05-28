2edbf11568703b5856b378871d672e585f350c47
jatin-dev-meesra <jatin.meesra.com>
2024-05-28 15:07:00 +0530

    carousel edit

--- src/components/carousel/carousel2.tsx
+++ src/components/carousel/carousel2.tsx
@@ -72,3 +72,93 @@ const CarouselComponent2 = () => {
 };
 
 export default CarouselComponent2;
+
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// import React, { useState } from "react";
+// import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+// import { RxDotFilled } from "react-icons/rx";
+
+// const CarouselComponent2 = () => {
+//   const slides = [
+//     {
+//       url: "https://images.unsplash.com/photo-1529592829369-5e84d19ca37a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTk4MTU3fHxlbnwwfHx8fHw%3D",
+//     },
+//     {
+//       url: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+//     },
+//     {
+//       url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
+//     },
+//     {
+//       url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+//     },
+//     {
+//       url: "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
+//     },
+//     {
+//       url: "https://images.unsplash.com/photo-1596683858875-63d85de92743?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     },
+//     {
+//       url: "https://images.unsplash.com/photo-1659926532321-5aa4ca41f0b5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     },
+//   ];
+
+//   const [currentIndex, setCurrentIndex] = useState(0);
+
+//   const prevSlide = () => {
+//     const isFirstSlide = currentIndex === 0;
+//     const newIndex = isFirstSlide
+//       ? Math.max(0, slides.length - 3)
+//       : currentIndex - 3;
+//     setCurrentIndex(newIndex);
+//   };
+
+//   const nextSlide = () => {
+//     const isLastSlide = currentIndex >= slides.length - 3;
+//     const newIndex = isLastSlide ? 0 : currentIndex + 3;
+//     setCurrentIndex(newIndex);
+//   };
+
+//   const goToSlide = (slideIndex: number) => {
+//     setCurrentIndex(slideIndex);
+//   };
+
+//   return (
+//     <>
+//       <div className="max-w-[1400px] h-full w-full pb-5 m-auto relative group">
+//         <div className="flex gap-2">
+//           {slides.slice(currentIndex, currentIndex + 3).map((slide, index) => (
+//             <div
+//               key={index}
+//               style={{ backgroundImage: `url(${slide.url})` }}
+//               className="w-1/3 h-96 rounded-xl bg-center bg-cover duration-500"
+//             ></div>
+//           ))}
+//         </div>
+//         {/* Left Arrow */}
+//         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+//           <BsChevronCompactLeft onClick={prevSlide} size={30} />
+//         </div>
+//         {/* Right Arrow */}
+//         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+//           <BsChevronCompactRight onClick={nextSlide} size={30} />
+//         </div>
+//         <div className="flex top-4 justify-center py-2">
+//           {slides.map((slide, slideIndex) => (
+//             <div
+//               key={slideIndex}
+//               onClick={() => goToSlide(slideIndex)}
+//               className="text-2xl cursor-pointer"
+//             >
+//               <RxDotFilled />
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default CarouselComponent2;
