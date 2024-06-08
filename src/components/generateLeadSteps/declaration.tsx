d515bd6bd6457da5164c60fd9ec5fc5aaeb90384
jatin-dev-meesra <jatin.meesra.com>
2024-06-08 10:59:15 +0530

    Payment Gateway

--- src/components/generateLeadSteps/declaration.tsx
+++ src/components/generateLeadSteps/declaration.tsx
@@ -22,53 +22,42 @@ const Declaration = () => {
       <h1 className=" text-xl font-medium">Declaration</h1>
       <div className="text-sm pt-5">
         <p>
-          I here by declare that the information provided in the above
-          questionnaire is true to the best of my knowledge. I confirm that the
-          answers I have given are, to the best of my knowledge, true, and that
-          have not withheld any material information that may influence the
-          assessment or acceptance of this application. I agree that this form
-          will constitute part of my application for insurance (s) and that
-          failure to disclose any material fact know to me may invalidate my
-          insurance(s).
+          I, hereby declare that the information provided in this form or
+          otherwise is true to the best of my knowledge and that I have not
+          withheld any material information that may influence the assessment or
+          acceptance or of this application. I agree that this form will
+          constitute part of my application for insurance(s) and that failure to
+          disclose any material fact known to me may invalidate my insurance(s).
+          I understand that in case of fraud or misrepresentation by me, the
+          Policy shall be cancelled immediately by paying surrender value, if
+          any, subject to the same being established by the Company in
+          accordance with section 45 of the Insurance Act 1938. I undertake to
+          make available to the Company and or its representatives such records,
+          documents etc. related as may be required in relation to this form as
+          and when requested for and that the Company may share this information
+          with such third parties as it may deem necessary in relation to
+          processing this form and the proposed policy.
         </p>
-        <br />
         <p>
-          I/We fully understand that any personal information collected or held
-          by the Bajaj Allianz Life Insurance Company Limited {`("Company")`}
-          (whether contained in this application or otherwise obtained) may be
-          held, used and disclosed by the Company to reinsurance companies,
-          claims investigation agencies, credit/claim/fraud bureaus or service
-          providers or repositories and relevant industry
-          associations/federations for the purpose of underwriting or claims
-          processing or for any analysis.
-        </p>
-        <br />
-        <p>
-          I/We am/are aware that the policy shall be governed by the Terms &
-          Conditions of the policy issued by BAJAJ ALLIANZ LIFE INSURANCE CO.
-          LTD. Pursuant to the proposal for insurance made by us. I/We have
-          independently verified the information before making my/our decision.
-        </p>
-        <br />
-        <p>
-          I/We am/are aware that the GCPP policy taken by me/us, is issued and
-          underwritten by BAJAJ ALLIANZ LIFE INSURANCE CO. LTD., and that all
-          claims will be settled BAJAJ ALLIANZ LIFE INSURANCE CO. LTD., as per
-          the terms and conditions of the policy.
-        </p>
-        <br />
-        <p>
-          I/We hereby confirm that I/We have agreed to subscribe to the policy
-          purely on a voluntary basis after taking my/our independent
-          professional advise and liable for any liability for loss or damage of
-          whatsoever nature, which may be attributable to payment of claims
-          under the policy of Insurance.
-        </p>
-        <br />
-        <p>
-          The above declaration and other details are true to best of my
-          knowledge. I have been explained the rules of the scheme and have
-          understood them.
+          I/we confirm that the person(s) named above as my/our Nominee have
+          been nominated in accordance with my/our statutory right of nomination
+          as contained in Section 39 of the Insurance Act, 1938. I/we hereby
+          declare and warrant that all these nominations have been made with
+          my/our free will and consent. I/we understand and agree that in the
+          unfortunate event of my/our death before I/we have re-paid the entire
+          outstanding loan amount, the outstanding loan amount as per the
+          benefit schedule and subject to the limit of the Coverage Sum Assured
+          will be paid to The Master Policy Holder, and any additional amount
+          payable under the policy after this payment has been made, subject to
+          the Coverage Sum Assured, will be payable to my/our other Nominees
+          named above or to my/our legal heirs or legal representatives (as
+          applicable). I/we understand and agree that no other nominee appointed
+          in accordance with Section 39 of the Insurance Act, 1938 or any of
+          my/our our legal heirs or legal representative will have the right to
+          receive or claim the amount (in whole or in part) which becomes due to
+          or payable to The Master Policy Holder, under the policy in the
+          unfortunate event of my/our death before I/we have re-paid the entire
+          outstanding loan amount.
         </p>
       </div>
       <div className="py-5">
